874c601b57750dd8d3b7c4a9062426048848c9d4
Lee Faria <leefaria@Lees-MacBook-Pro.local>
2020-01-29 16:37:23 -0500

    passing three tests

--- contracts/EventTickets.sol
+++ contracts/EventTickets.sol
@@ -7,10 +7,13 @@ pragma solidity ^0.5.0;
 contract EventTickets {
 
     /*
+
         Create a public state variable called owner.
         Use the appropriate keyword to create an associated getter function.
         Use the appropriate keyword to allow ether transfers.
      */
+
+     address payable public owner ;
 
     uint   TICKET_PRICE = 100 wei;
 
@@ -21,6 +24,17 @@ contract EventTickets {
         The "buyers" field should keep track of addresses and how many tickets each buyer purchases.
     */
 
+    struct Event{
+        string description;
+        string website;
+        uint totalTickets;
+        uint sales;
+        mapping(address => uint) buyers;
+        bool isOpen;
+        
+
+    }
+
     Event myEvent;
 
     /*
@@ -30,9 +44,19 @@ contract EventTickets {
         LogEndSale should provide infromation about the contract owner and the balance transferred to them.
     */
 
+    event LogBuyTickets(address _buyers, uint _tkspurchased);
+    event LogGetRefund(address _requester, uint _tksrefund);
+    event LogEndSale(address _owner, uint _balance);
+
+
     /*
         Create a modifier that throws an error if the msg.sender is not the owner.
     */
+
+   modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
 
     /*
         Define a constructor.
@@ -41,15 +65,29 @@ contract EventTickets {
         Set the appropriate myEvent details.
     */
 
+    constructor(string memory description, string memory URL, uint sales) public {
+        owner = msg.sender;
+        myEvent = Event({description: description, website:URL, totalTickets: 100, sales: 0, isOpen:true });
+
+
+    }
+
     /*
         Define a function called readEvent() that returns the event details.
         This function does not modify state, add the appropriate keyword.
         The returned details should be called description, website, uint totalTickets, uint sales, bool isOpen in that order.
     */
     function readEvent()
-        public
-        returns(string memory description, string memory website, uint totalTickets, uint sales, bool isOpen)
+        public view 
+        returns(string memory description, string memory website, uint totalTickets, uint sales, bool isOpen) 
     {
+        description = myEvent.description;
+        website = myEvent.website;
+        totalTickets = myEvent.totalTickets;
+        sales = myEvent.sales;
+        isOpen = myEvent.isOpen;
+        return (description, website, totalTickets, sales, isOpen);
+        
 
     }
 
@@ -58,6 +96,15 @@ contract EventTickets {
         This function takes 1 argument, an address and
         returns the number of tickets that address has purchased.
     */
+
+     function getBuyerTicketCount(address buyeraddress)
+        public
+        returns(uint ticketCount)
+    {
+    
+       return myEvent.buyers[buyeraddress];
+
+    }
 
     /*
         Define a function called buyTickets().
